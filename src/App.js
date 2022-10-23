534a1a60a4a72d0d213cdace04438cf5b16f023c
tina <danatina.dt@gmail.com>
2022-10-23 11:04:04 +0200

    add a note map, import notes.js doc

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './style.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Note from './components/Note';
+import notes from './notes';
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map( note => ( <Note
+        key = {note.id}
+        title = {note.title}
+        content = {note.content}
+      />
+      ))}
       <Footer />
     </div>
 
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,9 @@
-export default function Note() {
+export default function Note(props) {
     return (
         <div className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
+            <p>{props.id}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
